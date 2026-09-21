f256a4d6e7e91e562afd3a58a49e1a4780a34995
sangmin <sonsm0928@gmail.com>
2026-09-21 11:50:35 +0900

    practice grayscale

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-from pathlib import Path
 import cv2
 
+color = cv2.imread('road.jpg')
+gray = cv2.imread('road.jpg', cv2.IMREAD_GRAYSCALE)
 
-img = cv2.imread('road.jpg')
-
-if img is None:
+if color is None or gray is None:
     # if image doesn't exist than add process   
     raise FileNotFoundError('road.jpg')
 
-print(img.shape, img.dtype)
-cv2.imshow('IMG',img)
+print(color.shape, gray.dtype)
+cv2.imshow('COLOR',color)
+cv2.imshow('GRAY',gray)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
