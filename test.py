ee3a6b54fbeb6a390905a4a407a4db4acf793f94
sangmin <sonsm0928@gmail.com>
2026-09-21 11:37:11 +0900

    change test.py for load image

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
+from pathlib import Path
 import cv2
 
 
-img = cv2.imread('img/road.jpg')
+img = cv2.imread('road.jpg')
+
+if img is None:
+    # if image doesn't exist than add process   
+    raise FileNotFoundError('road.jpg')
+
+print(img.shape, img.dtype)
+cv2.imshow('IMG',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
